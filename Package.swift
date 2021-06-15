27a0be5a8c4b4c68d3bda8f7c61830bfedec7d77
Jonathan Hemi <jonathan@hemi.dev>
2021-06-15 16:08:34 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+	name: "Clibgit2",
+	platforms: [
+		.iOS(.v13)
+	],
+	products: [
+		.library(name: "Clibgit2", targets: ["Clibgit2"]),
+	],
+	dependencies: [],
+	targets: [
+		.binaryTarget(
+			name: "Clibgit2",
+			url: "https://github.com/yonihemi/Clibgit2/files/6653766/Clibgit2.xcframework.zip",
+			checksum: "4fb7ed123189d30ce8b685abf1a5fcdb252f5ad439df67a95effd3942a5ea5b9"
+		),
+	]
+)
